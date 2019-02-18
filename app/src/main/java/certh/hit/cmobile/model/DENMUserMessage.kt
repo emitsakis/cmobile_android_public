6adffb9375137e20c7497f68a44715b268bcf169
Unknown <cmobiledev@gmail.com>
2019-02-18 09:14:22 +0200

    working progress

---
+++ app/src/main/java/certh/hit/cmobile/model/DENMUserMessage.kt
@@ -0,0 +1,6 @@
+package certh.hit.cmobile.model
+
+/**
+ * Created by anmpout on 18/02/2019
+ */
+class DENMUserMessage : UserMessage()
---
+++ app/src/main/java/certh/hit/cmobile/model/EgnatiaUserMessage.kt
@@ -0,0 +1,11 @@
+package certh.hit.cmobile.model
+
+/**
+ * Created by anmpout on 18/02/2019
+ */
+class EgnatiaUserMessage : UserMessage() {
+    internal var name: `var`? = null
+    internal var trafficSingDescription: `var`? = null
+    internal var ServiceCategoryCode: `var`? = null
+    internal var serviceCategory: `var`? = null
+}
